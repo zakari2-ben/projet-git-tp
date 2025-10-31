bbe39702edc0c2743eb33b3186fda2bacfa7f59a
zakari2a-ben <riariazakaria6@gmail.com>
2025-11-01 00:37:05 +0100

    ajoute du fichier principal

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-#!/ usr/ bin / env python 3 # -*- coding : utf -8 -*-
-
-def main ():
-    print(" Bonjour␣Git!")
-if __name__ == " __main__": 
+#!/ usr / bin / env python3
+# -* - coding : utf -8 -* -
+def main () :
+    print (" Bonjour ␣ Git !")
+if __name__ == " __main__ ":
     main ()
-
-def afficher_version ():
-    print(" Version ␣ 1.0 ")
